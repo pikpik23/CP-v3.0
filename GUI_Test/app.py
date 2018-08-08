49d4e2107444661664e7d252faa0fbe0812b1a41
Trent <tmuskens19@knox.nsw.edu.au>
2018-08-08 21:25:51 +1000

    wtf

--- GUI_Test/app.py
+++ GUI_Test/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return 'INDEX2'
 
 
 
