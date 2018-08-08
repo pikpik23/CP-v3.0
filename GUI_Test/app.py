18cb123893426d9b8f459406994ddd13706e519e
Trent <tmuskens19@knox.nsw.edu.au>
2018-08-08 21:23:53 +1000

    testing gui

--- GUI_Test/app.py
+++ GUI_Test/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'INDEX2'
+    return render_template('home.html')
 
 
 
