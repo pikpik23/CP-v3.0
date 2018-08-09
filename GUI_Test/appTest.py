e6645c062b2237b3292c74fe6e9cce48eb90ce0a
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-09 14:00:35 +1000

    working input from maintdem form

--- GUI_Test/appTest.py
+++ GUI_Test/appTest.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from read_dictionary import dictionary
 
 app = Flask(__name__)
@@ -11,9 +11,37 @@ def index():
 
 @app.route('/table')
 def display_table():
-    lel = "LOCSTAT"
-    x = dictionary.read()
-    return render_template('tables_test.html', serials_def=x)
+    return render_template('tables_test.html', serials_def=dictionary.read())
+
+
+@app.route('/return')
+def return_page():
+    return render_template('return.html')
+
+
+@app.route('/return', methods=['POST'])
+def get_data():
+
+    text = request.form["content"]
+    print(text)
+    processed_text = text.upper()
+    return processed_text
+
+
+@app.route('/maintdem')
+def maint_test():
+    return render_template('maint_test.html')
+
+
+@app.route('/maintdem', methods=['POST'])
+def maint_test_return():
+
+    ret = {}
+    for serial in dictionary.read()["MAINTDEM"]:
+        ret.update({serial: request.form[serial]})
+    print(ret)
+
+    return render_template('return_display_test.html', serials_def=ret)
 
 
 if __name__ == '__main__':
