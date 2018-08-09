b38ed23e835b30a5e023ffd46001de450da53c75
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-09 17:54:22 +1000

    put my html in different folder

--- GUI_Test/appTest.py
+++ GUI_Test/appTest.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from read_dictionary import dictionary
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='test_templates')
 
 
 @app.route('/')
