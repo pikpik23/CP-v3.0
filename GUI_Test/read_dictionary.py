57e831820aab234c07a87a3191d73d1412d25437
Trent <tmuskens19@knox.nsw.edu.au>
2018-08-11 07:16:19 +1000

    created all new serials
    it was a trek

--- GUI_Test/read_dictionary.py
+++ GUI_Test/read_dictionary.py
@@ -82,10 +82,276 @@ class dictionary:
 
 
 
+                'STRENGTHSTATE': {
+                    "A1": {
+                        "desc": "No. of Own Cadets",
+                        "data_type": "string"
+                    }, 
+
+                    "A2": {
+                        "desc": "No. of Attachments",
+                        "data_type": "string",
+                    },  
+
+                    "A3": {
+                        "desc": "No. of Staff",
+                        "data_type": "string"
+                    }
+
+                    "B1": {
+                        "desc": "No. of expected attachments in next 24 hours",
+                        "data_type": "string"
+                    }
+
+                    "B2": {
+                        "desc": "No. of expected detachments in next 24 hours",
+                        "data_type": "string"
+                    }
+
+                    "C": {
+                        "desc": "Total Personnel",
+                        "data_type": "string"
+                    }
 
 
 
+                'CASEVAC': {
+                    "PRI": {
+                        "desc": "Priority",
+                        "data_type": "choice",
+                        "options": ["Pri 1", "Pri 2", "Pri 3"]
 
+                    "A": {
+                        "desc": "Patient ID",
+                        "data_type": "string"
+                    }, 
+
+                    "B1": {
+                        "desc": "No. of Stretcher Cases",
+                        "data_type": "string",
+                    },  
+
+                    "B2": {
+                        "desc": "No. of Sitting Cases",
+                        "data_type": "string"
+                    }
+
+                    "C": {
+                        "desc": "Requirements for special equipment",
+                        "data_type": "long"
+                    }
+
+                    "D1": {
+                        "desc": "Location of RV",
+                        "data_type": "location"
+                    }
+
+                    "D2": {
+                        "desc": "Callsign and Channel at RV",
+                        "data_type": "string"
+                    }
+
+                    "E": {
+                        "desc": "Remarks",
+                        "data_type": "long"
+                    }    
+                }
+
+
+                'NOTICAS': {
+                    "A": {
+                        "desc": "Patient ID",
+                        "data_type": "string"
+                    }, 
+
+                    "B": {
+                        "desc": "Rank",
+                        "data_type": "string",
+                    },  
+
+                    "C": {
+                        "desc": "Name",
+                        "data_type": "string"
+                    }
+
+                    "D": {
+                        "desc": "Details of injury or illness",
+                        "data_type": "long"
+                    }
+
+                    "E": {
+                        "desc": "Location of injury",
+                        "data_type": "location"
+                    }
+
+                    "F": {
+                        "desc": "Time of injury",
+                        "data_type": "string"
+                    }
+
+                    "G": {
+                        "desc": "Treatment administered",
+                        "data_type": "long"
+                    }
+
+                    "H": {
+                        "desc": "Remarks and Present Location",
+                        "data_type": "long"
+                    }
+
+                }
+
+                'SITREP': {
+                    "A": {
+                        "desc": "Time",
+                        "data_type": "string"
+                    }, 
+
+                    "B": {
+                        "desc": "Own Situation",
+                        "data_type": "long",
+                    },  
+
+                    "C": {
+                        "desc": "Situation with regards to third parties",
+                        "data_type": "long"
+                    }
+
+                    "D": {
+                        "desc": "Future intentions and relevant general information",
+                        "data_type": "long"
+                    }
+                }
+
+                'INTREP': {
+                    "A": {
+                        "desc": "Time of incident",
+                        "data_type": "string"
+                    }, 
+
+                    "B": {
+                        "desc": "Location of incident",
+                        "data_type": "location",
+                    },  
+
+                    "C": {
+                        "desc": "Brief description of incident",
+                        "data_type": "long"
+                    }
+
+                    "D": {
+                        "desc": "Commanders evaluation",
+                        "data_type": "long"
+                    }
+                }
+
+                'MOVEREQ': {
+                    "A": {
+                        "desc": "Callsign of person at pick-up location",
+                        "data_type": "string"
+                    }, 
+
+                    "B1": {
+                        "desc": "Location of pick up",
+                        "data_type": "location",
+                    },  
+
+                    "B2": {
+                        "desc": "Location of destination",
+                        "data_type": "location"
+                    }
+
+                    "C1": {
+                        "desc": "Number of persons to be transported",
+                        "data_type": "string"
+                    }
+
+                    "C2": {
+                        "desc": "Configuration of troops",
+                        "data_type": "choice"
+                        "options": ["Marching Order 1", "Patrol Order"]
+                    }
+
+                    "D1": {
+                        "desc": "Description of cargo",
+                        "data_type": "string"
+                    }
+
+                    "D2": {
+                        "desc": "Estimated number of vehicles",
+                        "data_type": "string"
+                    }
+
+                    "D3": {
+                        "desc": "Is a loading/unloading pary available at Loc",
+                        "data_type": "string"
+                    }
+
+                    "E": {
+                        "desc": "Time of pickup",
+                        "data_type": "string"
+                    }    
+                }
+
+                'STARLIGHTREQ': {
+                    "A": {
+                        "desc": "Callsign of person at location",
+                        "data_type": "string"
+                    }, 
+
+                    "B": {
+                        "desc": "Location",
+                        "data_type": "location",
+                    },  
+
+                    "C": {
+                        "desc": "Nature of illness or injury",
+                        "data_type": "long"
+                    }
+
+                    "D": {
+                        "desc": "Any RV details",
+                        "data_type": "long"
+                    }
+
+                    "E": {
+                        "desc": "Remarks",
+                        "data_type": "long"
+                    }
+                }
+
+                'SENTRYREP': {
+                    "A": {
+                        "desc": "Vehicle Number Plate",
+                        "data_type": "string"
+                    }, 
+
+                    "B": {
+                        "desc": "Status",
+                        "data_type": "choice",
+                        "options": ["Entering 1", "Leaving"]
+                    },  
+
+                    "C": {
+                        "desc": "Passengers",
+                        "data_type": "string"
+                    }
+
+                    "D": {
+                        "desc": "Destination",
+                        "data_type": "location"
+                    }
+
+                    "E": {
+                        "desc": "Estimated time of return",
+                        "data_type": "string"
+                    }
+
+                    "F": {
+                        "desc": "Remarksl",
+                        "data_type": "long"
+                    }
+                }
 
             }
 
