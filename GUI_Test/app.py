775d227fedbac40032919d8747ac63be83bc9b5c
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-12 13:14:37 +1000

    changed app name

--- GUI_Test/app.py
+++ GUI_Test/app.py
@@ -1,42 +1,50 @@
+"""
+The main file for the CP site
+
+All request handling is done from here
+"""
+
 from flask import Flask, render_template, request
 from read_dictionary import dictionary
 
-app = Flask(__name__, template_folder='templates',
+APP = Flask(__name__, template_folder='templates',
             static_folder='static', static_url_path='')
 
+LEGACY_DIC = dictionary.read_legacy()
+SERIALS = dictionary.read()
+LOCATIONS = dictionary.read_locations()
 
-@app.route('/')
+
+@APP.route('/')
 def index():
     return render_template('home.html', test_var='\nHi from python')
 
 
-@app.route('/table')
+@APP.route('/table')
 def display_table():
-    return render_template('tables_test.html', serials_def=legacy_dictionary)
+    return render_template('tables_test.html', serials_def=LEGACY_DIC)
 
 
-@app.route('/testIndex')
+@APP.route('/testIndex')
 def display_main():
-    return render_template('index_test.html', serials_def=legacy_dictionary)
+    return render_template('index_test.html', serials_def=LEGACY_DIC)
 
 
-@app.route('/testIndex/<rtrn_type>')
+@APP.route('/testIndex/<rtrn_type>')
 def abstracted_return(rtrn_type):
-    return render_template('abstracted_return.new.html', return_type=rtrn_type, serials_def=detailed_dictionary, locs=locations)
+    return render_template('abstracted_return.new.html',
+                    return_type=rtrn_type, serials_def=SERIALS, locs=LOCATIONS)
 
 
-@app.route('/textIndex/<rtrn_type>', methods=['POST'])
+@APP.route('/textIndex/<rtrn_type>', methods=['POST'])
 def abstracted_return_return(rtrn_type):
 
     ret = {}
-    for serial in legacy_dictionary[rtrn_type]:
+    for serial in LEGACY_DIC[rtrn_type]:
         ret.update({serial: request.form[serial]})
     return render_template('return_display_test.html', serials_def=ret)
 
 
 if __name__ == '__main__':
-    global legacy_dictionary, detailed_dictionary, locations
-    legacy_dictionary = dictionary.read_legacy()
-    detailed_dictionary = dictionary.read()
-    locations = dictionary.read_locations()
-    app.run(debug=True)
+
+    APP.run(debug=True)
