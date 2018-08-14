1073f5fa0afd21de1c25153eeacab706fb242e1a
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-14 16:24:55 +1000

    added time

--- GUI_Test/app.py
+++ GUI_Test/app.py
@@ -7,6 +7,7 @@ All request handling is done from here
 
 from flask import Flask, render_template, request, redirect
 from read_dictionary import dictionary
+from datetime import datetime
 
 APP = Flask(__name__, template_folder='resources/templates',
             static_folder='resources/static', static_url_path='')
@@ -16,35 +17,6 @@ SERIALS = dictionary.read()
 LOCATIONS = dictionary.read_locations()
 SETTINGS = dictionary.read_settings()
 LOG = []
-
-
-@APP.route('/log')
-def display_log():
-    return render_template("log_test.html", log=LOG)
-
-
-@APP.route('/log/<index>')
-def test_log(index):
-    try:
-        index = int(index)
-        return render_template("log_frame.html", ret=LOG[index])
-    except:
-        return "ERROR"
-
-
-@APP.route('/settings')
-def display_settings():
-    return render_template('settings_page.html', serials_def=SERIALS, locs=LOCATIONS, settings=SETTINGS)
-
-
-@APP.route('/notes')
-def display_notes():
-    return render_template('notes.html')
-
-
-@APP.route('/text')
-def display_text():
-    return render_template('texttest.html')
 
 
 @APP.route('/')
@@ -64,18 +36,14 @@ def abstracted_return(rtrn_type):
                            return_type=rtrn_type, serials_def=SERIALS, locs=LOCATIONS, settings=SETTINGS)
 
 
-@APP.route('/transmission/<rtrn_type>', methods=['POST'])
-def abstracted_return_return(rtrn_type):
-    ret = {}
-    ret.update({'name': rtrn_type})
-    ret.update({'sender': request.form['sender']})
-    ret.update({'reciver': request.form['reciver']})
+@APP.route('/notes')
+def display_notes():
+    return render_template('notes.html')
 
-    for serial in LEGACY_DIC[rtrn_type]:
-        ret.update({serial: request.form[serial]})
-    LOG.append(ret)
 
-    return render_template("log_frame.html", ret=ret)
+@APP.route('/text')
+def display_text():
+    return render_template('texttest.html')
 
 
 @APP.route('/settings/<setting>/', methods=['POST'])
@@ -85,8 +53,45 @@ def abstracted_updating_settings(setting):
     return redirect(request.form['page'])
 
 
+@APP.route('/settings')
+def display_settings():
+    return render_template('settings_page.html', serials_def=SERIALS, locs=LOCATIONS, settings=SETTINGS)
+
+
 def update_setting():
     dictionary.save_settings(SETTINGS)
+
+
+@APP.route('/transmission/<rtrn_type>', methods=['POST'])
+def abstracted_return_return(rtrn_type):
+    ret = {}
+    ret.update({'name': rtrn_type})
+    ret.update({'sender': request.form['sender']})
+    ret.update({'reciver': request.form['reciver']})
+    ret.update({'time': datetime.today().strftime('%d%H%M')})
+
+    if rtrn_type == "MESSAGE":
+        ret.update({'msg': request.form['msg']})
+    else:
+        for serial in LEGACY_DIC[rtrn_type]:
+            ret.update({serial: request.form[serial]})
+    LOG.append(ret)
+
+    return render_template("log_frame.html", ret=ret)
+
+
+@APP.route('/log')
+def display_log():
+    return render_template("log_test.html", log=LOG.reverse())
+
+
+@APP.route('/log/<index>')
+def test_log(index):
+    try:
+        index = int(index)
+        return render_template("log_frame.html", ret=(LOG.reverse())[index])
+    except:
+        return "<h1>ERROR</h1>"
 
 
 if __name__ == '__main__':
