91a126a9715f02f5b67208aed58d6b9a48499fac
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-14 10:16:18 +1000

    script

--- GUI_Test/run.sh
+++ GUI_Test/run.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-
-#echo launching web server
-#./app.py & 
-
-#sleep 3
-
-#echo launching Chrome
-#/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk --app=http://127.0.0.1:5000/
-
-./app.py & export python=$!
-sleep 3
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=http://127.0.0.1:5000/ & export chropid=$!
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk http://127.0.0.1:5000/ & export chropid=$!
 osascript -e 'tell application "Google Chrome" to activate'
-P2=$!
-wait $P2
-read -p "Press enter to kill"
-trap 'kill $(jobs -p)' EXIT
