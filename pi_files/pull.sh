576f646b1f6a5bd376979ec1810305b27a59dab8
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-15 20:08:35 +1000

    changed paths

--- pi_files/pull.sh
+++ pi_files/pull.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-cd /home/cp/cp
-git pull git@github.com:pikpik23/Google-API-Experiments
-
-
-echo "/GUI_Test/" >> .git/info/sparse-checkout
-
-git config --global user.email "30890484+pikpik23@users.noreply.github.com"
+cd ../
+git pull limited_origin main
--- pi_files/setup.sh
+++ pi_files/setup.sh
@@ -8,3 +8,6 @@ git remote add -f limited_origin git@github.com:pikpik23/Google-API-Experiments
 
 # Add files to be downloaded
 echo "GUI_Test/" >> .git/info/sparse-checkout
+echo "pi_files/" >> .git/info/sparse-checkout
+
+git pull limited_origin main
