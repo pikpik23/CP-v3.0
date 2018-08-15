dbcd88141305a2875ad6ebb1a93d2d5a66f3a8f3
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-15 20:05:39 +1000

    added pi scripts

---
+++ pi_files/pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/cp/cp
+git pull git@github.com:pikpik23/Google-API-Experiments
+
+
+echo "/GUI_Test/" >> .git/info/sparse-checkout
+
+git config --global user.email "30890484+pikpik23@users.noreply.github.com"
---
+++ pi_files/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git init
+git config core.sparseCheckout true
+
+# add remote
+git remote add -f limited_origin git@github.com:pikpik23/Google-API-Experiments
+
+# Add files to be downloaded
+echo "GUI_Test/" >> .git/info/sparse-checkout
