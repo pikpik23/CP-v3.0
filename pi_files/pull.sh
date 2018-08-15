f101ead260f4093c80003576b5657e19c80fcba6
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-08-15 21:23:10 +1000

    test

--- pi_files/pull.sh
+++ pi_files/pull.sh
@@ -2,13 +2,10 @@
 
 cd ../
 
-git read-tree -mu HEAD
-
-git fetch --all
-git reset --hard limited_origin/main
-
-
-git remote update
 git fetch limited_origin main
 
-sudo chmod 777 -R ../../
+git reset FETCH_HEAD
+
+git clean -df
+
+sudo chmod 777 -R ../
