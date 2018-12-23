0602b9a8172464b12a3cb3a3dc6a06dcf6fcfb53
pikpik23 <30890484+pikpik23@users.noreply.github.com>
2018-12-23 10:32:45 +0200

    save to usb

--- GUI_Test/test_app.py
+++ GUI_Test/test_app.py
@@ -1,24 +1,52 @@
 from app import *
+from glob import glob
+import os
+import shutil
+
+print(glob("/Volumes/*"))
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 def test_function():
-    lname = reset_log()
+    files = glob("/Volumes/*")
 
-    path = 'resources/static/LOG_'
-    extension = '.db'
-    lname = lname.strip(path) # strip the path
-    lname = lname.strip(extension) # strip db extension
+    rem = ["/Volumes/student", "/Volumes/com.apple.TimeMachine.localsnapshots", "/Volumes/Macintosh HD", "Blah"]
 
-    try:
-        lname = float(lname)
-        lname += 1
-    except ValueError:
-        pass
+    for path in rem:
+        try:
+            files.remove(path)
+        except ValueError:
+            pass
 
+    print(files)
 
-    new_lname = path+str(lname)+extension
+    usb = None
+    for path in files:
+        if "CP" in path:
+            usb = os.path.join(path, "Backup")
+        else:
+            usb = None
+            print("No Backup USB found")
 
-    print(new_lname)
+    if usb:
+        # save to usb
+        print("Saving...")
+        save(os.path.join(usb, "files"), "resources/files")
+        save(os.path.join(usb, "db"), "resources/static/db")
+        print("...Saved")
 
+def save(dest, src):
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+        copytree(src, dest)
 
 if __name__ == '__main__':
     test_function()
